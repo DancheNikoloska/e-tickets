a8df49b0780b02b089c42bfbd06c0ff7ddcd37c3
Danche <danchenikoloska@gmail.com>
2014-12-07 20:11:17 +0100

    has_ticket of majko

--- developement/init.php
+++ developement/init.php
@@ -107,19 +107,7 @@ function seedEntities($link) {
 	(null,6,3,5)";
 	mysqli_query($link, $query);
 	
-	//insert into has_ticket
 	
-	$query="INSERT INTO has_ticket VALUES
-	(4,25),(4,26),(4,27),(2,40),(2,41),(2,44),(2,45),(3,100),(3,101),(3,115),(3,120),(4,121),(3,122),(4,123),(2,124),(5,135),
-	(5,136),(5,137),(5,138),(5,139),(5,160),(4,161),(4,162),(2,166),(2,168),(2,170)";
-	if (mysqli_query($link, $query))
-	{
-		echo "ok";
-	}
-else
-{
-	echo "GRESKA:" .mysqli_error($link);
-}
 	
 	
 	
@@ -153,7 +141,19 @@ else
 		mysqli_query($link, "INSERT INTO tickets VALUES (null,'fm$i', N'Партер',300,5)");
 	} 
 	
-
+//insert into has_ticket
+	
+	$query="INSERT INTO has_ticket VALUES
+	(4,25),(4,26),(4,27),(2,40),(2,41),(2,44),(2,45),(3,100),(3,101),(3,115),(3,120),(4,121),(3,122),(4,123),(2,124),(5,135),
+	(5,136),(5,137),(5,138),(5,139),(5,160),(4,161),(4,162),(2,166),(2,168),(2,170)";
+	if (mysqli_query($link, $query))
+	{
+		echo "ok";
+	}
+else
+{
+	echo "GRESKA:" .mysqli_error($link);
+}
 	
 }
  
