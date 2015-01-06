8c7077169948b806b91eeecf4aa4f01d320b6085
marija92 <gramatikova.marija@gmail.com>
2015-01-06 17:33:51 +0100

    little change ^_^

--- shoppingCart.php
+++ shoppingCart.php
@@ -118,8 +118,8 @@
 									<div class="col-xs-4">									
 										<!--PayPal integracija-->
 										<form class="form" action="Checkout/paypal_ec_redirect.php" method="POST">
-									      <input type="hidden" name="PAYMENTREQUEST_0_AMT" value="10.00"></input>
-									      <input type="hidden" name="currencyCodeType" value="USD"></input>
+									      <input type="hidden" name="PAYMENTREQUEST_0_AMT" value='<?php echo $totalPrice; ?>'></input>
+									      <input type="hidden" name="currencyCodeType" value="MKD"></input>
 									      <input type="hidden" name="paymentType" value="Sale"></input>
 									      <!--Pass additional input parameters based on your shopping cart. For complete list of all the parameters click here -->
 									      <input type="image" src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/checkout-logo-large.png" alt="Check out with PayPal"></input>
