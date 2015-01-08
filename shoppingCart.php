2c483c6f14e57fbb9355f429bf99102a71fe2925
marija92 <gramatikova.marija@gmail.com>
2015-01-08 15:00:06 +0100

    generalka2 :satisfied:

--- shoppingCart.php
+++ shoppingCart.php
@@ -118,15 +118,13 @@
 									<div class="col-xs-4">									
 										<!--PayPal integracija-->
 										<form action="Checkout/paypal_ec_redirect.php" method="POST">
-     <input type="hidden" name="PAYMENTREQUEST_0_AMT" value='<?php echo $totalPrice; ?>'></input>
-      <input type="hidden" name="currencyCodeType" value="USD"></input>
-      <input type="hidden" name="paymentType" value="Sale"></input>
-      <!--Pass additional input parameters based on your shopping cart. For complete list of all the parameters click here -->
-      <input type="image" src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/checkout-logo-large.png" alt="Check out with PayPal"></input>
-</form>
-										
-										
-										</div>
+										      <input type="hidden" name="PAYMENTREQUEST_0_AMT" value='<?php echo $totalPrice; ?>'></input>
+										      <input type="hidden" name="currencyCodeType" value="USD"></input>
+										      <input type="hidden" name="paymentType" value="Sale"></input>
+										      <!--Pass additional input parameters based on your shopping cart. For complete list of all the parameters click here -->
+										      <input type="image" src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/checkout-logo-large.png" alt="Check out with PayPal"></input>
+										</form>										
+									</div>
 								</div>
 							</div>
 							
